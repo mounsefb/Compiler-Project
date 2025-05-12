aad01d40c3a54c70771ddbc68f7ece526fbbcb5b
mounsefb <mounsef.bouhar@gmail.com>
2025-05-12 10:56:01 +0200

    cleaning compiled file

--- global/sources/Machine_Abstraite/L_Interp/Src/arrondis.ads
+++
@@ -1,5 +0,0 @@
-package Arrondis is
-   type Mode_Arrondi is (FE_TONEAREST, FE_UPWARD, FE_DOWNWARD, FE_TOWARDZERO);
-   procedure Positionner_Arrondi_Flottant(Mode : Mode_Arrondi);
-   Fe_Error : exception;
-end Arrondis;
